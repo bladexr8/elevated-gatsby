819c2298fe1cb17bb1c81b2c9b884525ac0dba87
Stephen Moss <bladexr8@gmail.com>
2022-12-14 09:25:51 +1100

    base gatsby files and config

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+// gatsby core configuration file
+// can contain plugins, metadata, styling
+// and offline support 
+
+module.exports = {
+  plugins: [],
+}
---
+++ gatsby-node.js
@@ -0,0 +1,1 @@
+// code to run during site building process
